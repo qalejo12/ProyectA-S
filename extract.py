1b9c6d5352fbd802b908f74180a0ca3c9ce2aa2f
Alejo1201 <equinterogiraldo12@gmail.com>
2023-04-08 16:57:43 -0500

    Change

--- extract.py
+++ extract.py
@@ -1,4 +1,19 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://fastapi-selenium-production-24c4.up.railway.app/")
+
+time.sleep(100)
+
 
